ee60851c34cf6a47afb8377259ff3fa4670fe968
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-11 22:33:19 +0200

    Add module to show how to assign values to variables

---
+++ 3_variables.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+age=35
+print("I am", age, "years old")
+
+age+=1
+print("And now I am", age)
+
+age = 45 +2
+
+print(age)
+
+
+# the list will be broken to the single variables x = 3 and y = 5
+x,y = (3,5)
+print("x is ",x)
+print("y is ",y)
