8b2988e48acd88fb9de43fdfbafe8657b21bf4cf
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-11 22:32:36 +0200

    Add module to show how to work with basic math funktions

---
+++ 2_base_math.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+import math
+
+print("3+1=",1+3)
+
+print("3-1=",3-1)
+
+print("3*2=",3*2)
+
+print("5/2=",5/2) # float division
+
+print("5//2=",5//2) # int division
+
+
+print("5%2=", 5%2)
+
+print("4**4=", 4**4)
+
+print("sqrt(16) =",math.sqrt( 16 ))
+
+
