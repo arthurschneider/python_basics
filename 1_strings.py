e9746a64776e8d7ba197019e00ef7cdd5a181093
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-11 22:31:50 +0200

    Add module which show how to work with strings and print statements

---
+++ 1_strings.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+
+print("Hello World")
+print('Hello Arthur')
+
+print()
+print('"Hello"')
+print("We're someone")
+print('We\'re anyone')
+
+
+print()
+print("A text over\
+        two lines")
+
+
+print("""Another version of
+        text over two
+        or more lines""")
+
+print('''You can print
+        with single quotes too''')
+
+print()
+print("3+4=", "7")
+print("3+4="+"7")
+
+print()
+print("2+6=", 8)
+print("2+6=" + str(8))
