110696e62681ef38bf4b8bb6129d63ff3f42a308
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-11 22:36:35 +0200

    Add module to show if else statements

---
+++ 6_if_else_statements.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+x = 2
+y = 3
+
+if x == y:
+    print("x is equal to y")
+else:
+    if x > y:
+        print("x is greater than y")
+    else:
+        print("x is less than y")
+
