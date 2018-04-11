242eb3119e1df7ddc7b2cfbf1911597bd335356b
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-11 22:35:36 +0200

    Add module with if statements

---
+++ 5_if_statments.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+x = 2
+y = 3
+a = 4
+
+if x > y:
+    print("x is greater than y")
+
+if x<y:
+    print("x is less than y")
+
+if x == y:
+    print("x is equal to y")
+
+if x != y:
+    print("x is not equal to y")
+
+if x >=y:
+    print("x is greater or equal to y")
+
+if x <= y:
+    print("x is less or equal to y")
+
+if y<a>x:
+    print("a is bigger than x and y")
+
+if "a"<"z":
+    print("Comparing string is also possible")
+
+
