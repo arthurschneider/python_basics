f26ee9f431f7df9cc144b434144104a023b87db0
Arthur Schneider <dev.schneiart@gmail.com>
2018-04-23 23:20:12 +0200

    Update statistics script

--- 14_statistics.py
+++ 14_statistics.py
@@ -2,7 +2,7 @@
 
 import statistics
 
-example_list = [3,4,2,8,8,5,9,6,7,3]
+example_list = [3,1,1,4,2,8,5,9,6,7,1]
 
 x = statistics.mean(example_list)
 print(x)
@@ -12,8 +12,8 @@ y = statistics.median(example_list)
 print(y)
 
 
-#z = statistics.mode(example_list)
-#print(z)
+z = statistics.mode(example_list)
+print(z)
 
 
 a = statistics.stdev(example_list)
